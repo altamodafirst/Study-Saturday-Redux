1114fdcc38d52db6be3891ec57e4357ea3bcb2d4
Amber Nifong <amber.nifong@gmail.com>
2022-02-05 11:30:58 -0500

    first section working

--- client/redux/store.js
+++ client/redux/store.js
@@ -2,20 +2,40 @@ import {createStore, applyMiddleware} from 'redux';
 import loggerMiddleware from 'redux-logger';
 import axios from 'axios';
 import thunkMiddleware from 'redux-thunk';
-
+// import {Main} from '../components/main'
+// import SingleStudent from '../components/SingleStudent'
+// import StudentList from '../components/StudentList';
 // ACTION TYPES go here:
-
+const GETTING_STUDENTS = 'GETTING_STUDENTS'
 
 // ACTION CREATORS go here:
+//student has id, fullName, email
+const _gettingStudents = (students) => {
+	return {
+		type: GETTING_STUDENTS,
+		students,
+	};
+};
 
+console.log("did we get students----", _gettingStudents);
 
 // THUNK CREATORS go here:
+export const fetchStudents= () => {
+	return async (dispatch) => {
+		const { data} = await axios.get('/api/students');
+		dispatch(_gettingStudents(data));
+	};
+};
 
-
-const initialState = {};
+const initialState = {
+  students: []
+};
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case GETTING_STUDENTS: {
+      return {students: action.students}
+    }
     default:
       return state;
   }
@@ -24,7 +44,6 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 // dispatch your own actions here to test your store functionality:
-store.dispatch({type: 'HELLO_WORLD'})
 
 // ^ you can see the logs above in your console, thanks to redux-logger!
 
